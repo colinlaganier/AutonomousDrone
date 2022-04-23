6f1011ce5d983bed01acecb803480bc896d5c829
colin-lag <34300599+colin-lag@users.noreply.github.com>
2022-04-23 17:28:09 +0100

    Added Client test code

---
+++ DroneClient/cmake-build-debug/CMakeFiles/DroneClient.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DroneClient.dir/main.cpp.o"
+  "DroneClient"
+  "DroneClient.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DroneClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
