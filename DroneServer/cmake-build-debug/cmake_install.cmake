7fda661c8ad7b3cd66328d516e74355c33560a81
colin-lag <34300599+colin-lag@users.noreply.github.com>
2022-05-10 17:52:07 +0100

    Added base server codeé

--- DroneServer/cmake-build-debug/cmake_install.cmake
+++ DroneServer/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/ubuntu/CLionProjects/DroneStation
+# Install script for directory: /Users/colinlaganier/Documents/Imperial/Thesis/AutonomousDrone/DroneServer
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -27,19 +27,9 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
-# Install shared libraries without execute permission?
-if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "1")
-endif()
-
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
-endif()
-
-# Set default install directory permissions.
-if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/usr/bin/objdump")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -50,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/ubuntu/CLionProjects/DroneStation/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/colinlaganier/Documents/Imperial/Thesis/AutonomousDrone/DroneServer/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
